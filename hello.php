9638d45e66f2258cd725aa17f57e1ae713430552
nguyenhieptn <nguyenhieptn@yahoo.com>
2014-09-10 10:12:02 +0700

    reset

--- hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "hello git world";
-echo "hello git world2";
-echo "hello git world2 - Hieu edit";
-echo "Hello world!";
-echo "conflict here";
-echo "hello git world4";
-echo "hello git world5";
